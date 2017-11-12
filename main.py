c2be1cba598b2b1116999f3d12c3962e82331c5c
Daniel Tam <grilled_chips@hotmail.com>
2017-11-12 20:39:32 +1100

    Implements basic rate dictionary based on bid/ask prices

--- main.py
+++ main.py
@@ -13,5 +13,29 @@ client = BTCMarkets (api_key, private_key)
  
 #print client.order_create('AUD', 'LTC', 100000000, 100000000, 'Bid', 'Limit', '1')
 
-print client.get_market_tick('ETH','AUD')
+fiat_fee = 0.007
+crypto_fee = 0.0022
 
+pairs = (('ETH','AUD'), ('ETH', 'BTC'), ('BTC', 'AUD'))
+
+# So prices['ETH']['AUD'] is the conversion rate from ETH to AUD
+# So 1 ETH * prices['ETH']['AUD'] ~= 300
+# prices = {'AUD': {}, 'ETH': {}, 'BTC': {}}
+rates = {}
+for pair in pairs:
+  for ticker in pair:
+    if ticker not in rates:
+      rates[ticker] = {}
+
+for pair in pairs:
+  result = client.get_market_tick(pair[0], pair[1])
+  print(result['bestBid'])
+  print(result['bestAsk'])
+
+  rates[pair[0]][pair[1]] = result['bestAsk']
+  rates[pair[1]][pair[0]] = 1/result['bestBid']
+
+print(rates)
+
+# print client.get_market_tick('ETH','BTC')
+# print client.get_market_tick('BTC','AUD')
